5670f180ed1a0e427af46a70ad11c2830312a009
Rafafi35 <salinger.rafael@gmail.com>
2026-04-19 12:18:48 +0200

    add icons to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,35 @@
 import { NavLink } from "react-router-dom";
+import { House, Wrench, ShoppingCart, Component } from "lucide-react"
 
 export default function Navigation() {
   const linkStyle = ({ isActive }) =>
-    `m-4 p-2 rounded-xl transition-colors hover:bg-gray-100 ${
+    `m-4 p-2 rounded-xl transition-colors hover:bg-gray-100 inline-flex items-center gap-2 ${
       isActive ? "bg-gray-200" : ""
     }`;
 
   return (
     <nav className="sticky top-0 left-0 w-full bg-white border-b-2 border-gray-200">
-        <div className="flex font-bold justify-center">
-                <NavLink to="/" end className={linkStyle}>
-                    Home
-                </NavLink>
+      <div className="flex font-bold justify-center">
+        <NavLink to="/" end className={linkStyle}>
+          <House size={18} />
+          Home
+        </NavLink>
 
-                <NavLink to="/configure" end className={linkStyle}>
-                    Configure
-                </NavLink>
+        <NavLink to="/configure" end className={linkStyle}>
+          <Wrench size={18} />
+          Configure
+        </NavLink>
 
-                <NavLink to="/buy-parts" end className={linkStyle}>
-                    Buy Parts
-                </NavLink>
+        <NavLink to="/buy-parts" end className={linkStyle}>
+          <Component size={18} />
+          Buy Parts
+        </NavLink>
 
-                <NavLink to="/cart" end className={linkStyle}>
-                    Cart
-                </NavLink>
-        </div>
+        <NavLink to="/cart" end className={linkStyle}>
+          <ShoppingCart size={18} />
+          Cart
+        </NavLink>
+      </div>
     </nav>
   );
 }
-
