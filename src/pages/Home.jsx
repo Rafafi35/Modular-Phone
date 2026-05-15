a96c9a815b918a03a7c615c7095010689873d364
Rafafi35 <salinger.rafael@gmail.com>
2026-05-15 17:17:46 +0200

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 function Home() {
     return (
-        <h1 className="font-bold">Hello</h1>
+        <div>
+            <h1 className="flex justify-center mt-30 font-bold text-8xl">Modular Phone</h1>
+                <ul className="mt-10 font-bold text-gray-500 text-3xl">
+                    <li className="m-3 flex justify-center">Replace damaged Parts</li>
+                    <li className="m-3 flex justify-center">Upgrade favorite Parts</li>
+                    <li className="m-3 flex justify-center">Share needed Parts</li>
+                </ul>
+        </div>
     )
 }
 
