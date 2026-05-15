6eb7e60fb9888b066e5d85e825310bd96ed47056
Rafafi35 <salinger.rafael@gmail.com>
2026-05-15 15:26:31 +0200

    update items

--- src/data/items.js
+++ src/data/items.js
@@ -2,18 +2,18 @@ const items = {
     base: [
         {
             title: "Base A",
-            specs: ["A16 Chip", "8GB RAM", "128 GB Memory"],
-            price: 699,
+            specs: ["Dimensity 7300 Pro Chip", "60 Hz", "8GB RAM", "128 GB Storage"],
+            price: 400,
         },
         {
             title: "Base Pro",
-            specs: ["A16 Chip", "16GB RAM", "256 GB Memory"],
-            price: 799,
+            specs: ["Snapdragon 8 Elite", "120 Hz", "12GB RAM", "256GB Storage"],
+            price: 600,
         },
         {
             title: "Base Ultra",
-            specs: ["A16 Chip", "16GB RAM", "512 GB Memory"],
-            price: 899,
+            specs: ["Snapdragon 8 Elite", "120 Hz", "16GB RAM", "1TB Storage"],
+            price: 800,
         },
     ],
 
@@ -38,17 +38,17 @@ const items = {
     camera: [
         {
             title: "Basic Camera",
-            specs: ["12 MP", "Portrait Mode", "Full HD Video"],
+            specs: ["50 MP main", "8 MP ultra-wide", "Full HD video"],
             price: 59,
         },
         {
             title: "Pro Camera",
-            specs: ["48 MP", "OIS", "4K Video"],
+            specs: ["100 MP main", "50 MP telephoto", "8 MP ultra wide", "4K video"],
             price: 129,
         },
         {
             title: "Ultra Camera",
-            specs: ["108 MP", "OIS+", "8K Video"],
+            specs: ["200 MP main", "50 MP telephoto", "50 MP ultra-wide", "5 MP macro", "8K Video"],
             price: 199,
         },
     ],
@@ -61,12 +61,12 @@ const items = {
         },
         {
             title: "Sensor",
-            specs: [],
+            specs: ["Heart Rate", "SpO²", "Temperature"],
             price: 39,
         },
         {
             title: "Mini Screen",
-            specs: [],
+            specs: ["2.7 Inches", "120 Hz"],
             price: 59,
         },
     ],
